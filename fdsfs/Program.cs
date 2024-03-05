495f18cd9c1605c6a89d8a656f39d9e93a34663f
efewfnicole111 <154506004+efewfnicole111@users.noreply.github.com>
2024-03-05 14:11:35 +0800

    1.去掉填写RCCA界面里的Calculate Downtime选择 2.取消Attendance按钮，直接显示 Close按钮，同时将RCCA界面的Attendtime 拿掉

---
+++ fdsfs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fdsfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
