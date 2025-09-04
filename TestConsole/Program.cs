cb1e7b00df217043e3faaccb7bb9b4cf83624c3c
羊乜乜 <1846014330@qq.com>
2025-09-04 14:54:11 +0800

    修复手动上传Downtime,无法上传至eCalling的问题

---
+++ TestConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+string basicURL = "http://cnwuxm1tes05";
+string url = "/eCalling/InsertData2?comfrom=Downtime System&department=ME&machine=Bay54M010&ErrorCode=频繁抛料&ErrorDescription=呼叫ME，频繁抛料&Server=CNWUXPRD0752&isDowntime=true";
+RestClient client = new RestClient(basicURL);
+string result = client.PostWithOutBody(url);
+Console.WriteLine(result);
+
